d8d88a69f48dce4346f818ce33672429bf9cd59a
barnaholl <hollbarna@freemail.hu>
2020-09-09 12:52:00 +0200

    Init project

---
+++ README.old.md
@@ -0,0 +1,1 @@
+# quizpp-react
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+
+const App = () => {
+  return (<p>App</p>)
+  
+
+}
+
+export default App;
