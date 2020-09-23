a7d56c9527dc9d4c67064237d4eafaebc377a5a0
barnaholl <hollbarna@freemail.hu>
2020-09-23 16:25:37 +0200

    play button to game

--- src/Comonent/Layout/GameCard.js
+++ src/Comonent/Layout/GameCard.js
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from "react";
+import {Link} from "react-router-dom";
 
 const GameCard = (props) => {
+  const TAG="Tag:";
+  const DESCRIPTION="Description:";
   const game = props.game;
 
   return (
     <div style={cardStyle}>
       <h1>{game.title}</h1>
-      <p>{game.tag}</p>
+      <h2>{TAG} {game.tag}</h2>
+      <h2>{DESCRIPTION}</h2>
+      <h3>{game.description}</h3>
+      <Link style={playButtonContainerStyle} to="/">Play</Link>
+      
+
     </div>
   );
 };
@@ -14,12 +22,20 @@ const GameCard = (props) => {
 const cardStyle = {
   maxWidth: "1fr",
   minWidth:"20rem",
-  height: "20rem",
+  height: "25rem",
   backgroundColor: "dimgrey",
   border: "1px solid darkgoldenrod",
   borderRadius: "3px",
-  textAlign: "justify",
-  
+  textAlign: "justify", 
 };
+const playButtonContainerStyle = {
+  backgroundColor: "green",
+  cursor:PointerEvent,
+  color:"white",
+  fontSize:"2rem"
+};
+
+
+
 
 export default GameCard;
