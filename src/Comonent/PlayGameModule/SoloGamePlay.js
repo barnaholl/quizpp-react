6e9b274da90035a056db930c1a6885d573e8b2ee
barnaholl <hollbarna@freemail.hu>
2020-10-11 14:40:01 +0200

    reworked SoloGamePlay

--- src/Comonent/PlayGameModule/SoloGamePlay.js
+++ src/Comonent/PlayGameModule/SoloGamePlay.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
+
 const SoloGamePlay = (props) =>{
+
 
     const NUMBER_OF_QUESTIONS=3;
 
@@ -12,44 +14,40 @@ const SoloGamePlay = (props) =>{
     const [isVictory,setIsVictory]=useState(false);
     const [isActive,setIsActive]= useState(true);
 
-
     useEffect(() => {
         axios.get(`http://localhost:8762/game-session-handler/${sessionId}`)
-        .then((res) => {
-        setQuestionCounter(res.data.currentRound);
-        setIsActive(res.data.isActive);
-        axios.get(`http://localhost:8762/question-handler/${res.data.currentQuestion}`)
-        .then((res)=>{
-        setQuestion(res.data)});
-            
-    });
+            .then((res) => {
+                setQuestionCounter(res.data.currentRound);
+                setIsActive(res.data.isActive);
+                 axios.get(`http://localhost:8762/question-handler/render/${res.data.currentQuestion}`)
+                    .then((res)=>{
+                    setQuestion(res.data);  
+                    });    
+            });
     },[]);
 
-    const answeredWell = () => {
-        if(questionCounter==NUMBER_OF_QUESTIONS){
-            axios.put(`http://localhost:8762/game-session-handler/${sessionId}/${false}`)
-                .then((res)=>{
-                    setIsActive(res.data.isActive)
-                    setIsVictory(true)
-                    console.log("Victory");
-                })
+    const chooseAnswer= (answer) => {
+    axios.put(`http://localhost:8762/game-session-handler/${sessionId}/${answer}`)
+    .then((res)=>{
+        console.log(res.data);
+        setIsActive(res.data.isActive);
+        setQuestionCounter(res.data.currentRound)
+        if(res.data.currentRound>NUMBER_OF_QUESTIONS){
+            setIsVictory(true);
+            setIsActive(false);
+            axios.put(`http://localhost:8762/game-session-handler/setActive/${sessionId}/${false}`)
+            .then((res)=>{
+                console.log(res.data);
+            });
         }
         else{
-            axios.put(`http://localhost:8762/game-session-handler/${sessionId}/${true}`)
-                .then((res)=>{
-                    setIsActive(res.data.isActive)
-                    setQuestionCounter(res.data.currentRound)
-                    axios.get(`http://localhost:8762/question-handler/${res.data.currentQuestion}`)
-                    .then((res)=>{setQuestion(res.data)})
-                });
-        }
-      }
-      const answeredWrong = () => {
-        axios.put(`http://localhost:8762/game-session-handler/${sessionId}/${false}`)
-        .then((res)=>{
-            setIsActive(res.data.isActive)
-        });
-      }
+            axios.get(`http://localhost:8762/question-handler/render/${res.data.currentQuestion}`)
+            .then((res)=>{setQuestion(res.data)})
+        } 
+
+    
+    });
+    }      
 
     return(        
         question ? (
@@ -65,11 +63,12 @@ const SoloGamePlay = (props) =>{
                 <div className="questionContainer">
                     <p style={questionStyle}>{question.question}</p>
                 </div>
+                
                 <div className="answerContainer">
-                    <p onClick={answeredWell} style={answerStyle}>{question.correctAnswer}</p>
-                    <p onClick={answeredWrong} style={answerStyle}>{question.wrongAnswer1}</p>
-                    <p onClick={answeredWrong} style={answerStyle}>{question.wrongAnswer2}</p>
-                    <p onClick={answeredWrong} style={answerStyle}>{question.wrongAnswer3}</p>
+                    <p onClick={()=>chooseAnswer(question.answer1)} style={answerStyle}>{question.answer1}</p>
+                    <p onClick={()=>chooseAnswer(question.answer2)} style={answerStyle}>{question.answer2}</p>
+                    <p onClick={()=>chooseAnswer(question.answer3)} style={answerStyle}>{question.answer3}</p>
+                    <p onClick={()=>chooseAnswer(question.answer4)} style={answerStyle}>{question.answer4}</p>
                 </div>
             </>
             ) 
